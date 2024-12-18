cbbaee87ab8a2f190fc83e4eaded493a614747e0
Tom Draper <tomjdraper1@gmail.com>
2024-12-18 17:28:15 +0000

    feat: update notes page

--- Pages/Notes.cshtml.cs
+++ Pages/Notes.cshtml.cs
@@ -3,6 +3,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SecureNotes.Pages;
 
+    public class Note 
+    {
+        public DateTime Timestamp { get; set; }
+        public string Content { get; set; }
+    }
+
     public class NotesModel : PageModel
     {
         public string NoteID { get; set; }
@@ -12,5 +18,14 @@ namespace SecureNotes.Pages;
         {
             // The NoteID parameter will be automatically passed from the URL
             this.NoteID = NoteID;
+        }
+    
+        public List<Note> GetNotes(string NoteID) 
+        {
+            return new List<Note> 
+            {
+                new Note { Timestamp = DateTime.Now, Content = "This is a note" },
+                new Note { Timestamp = DateTime.Now, Content = "This is another note" }
+            };
         }
     }
